50a3e0764b15bc2d04c970630628d8ce6e424421
paf106 <pabloavila106@gmail.com>
2021-03-23 23:27:04 +0100

    Centered elements

---
+++ MyApp/app/src/main/java/com/MyGymRoutine/myapp/LogInActivity.java
@@ -0,0 +1,14 @@
+package com.MyGymRoutine.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_activity);
+    }
+}
