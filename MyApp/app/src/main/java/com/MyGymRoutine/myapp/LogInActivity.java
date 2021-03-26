b796cd620bc497505fe528276762707293b27177
paf106 <pabloavila106@gmail.com>
2021-03-26 16:48:04 +0100

    Some code added for register label

--- MyApp/app/src/main/java/com/MyGymRoutine/myapp/LogInActivity.java
+++ MyApp/app/src/main/java/com/MyGymRoutine/myapp/LogInActivity.java
@@ -3,12 +3,29 @@ package com.MyGymRoutine.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView tvCreateAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in_activity);
+        getSupportActionBar().hide();
+
+        tvCreateAccount = findViewById(R.id.tvCreateAccount);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tvCreateAccount:
+
+                break;
+        }
     }
 }
