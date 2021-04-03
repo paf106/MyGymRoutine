029e0411227b20ea7c67d1d73b730c7f90b0c16e
paf106 <pabloavila106@gmail.com>
2021-04-04 00:30:19 +0200

    some code added

--- MyApp/app/src/main/java/com/MyGymRoutine/myapp/LogInActivity.java
+++ MyApp/app/src/main/java/com/MyGymRoutine/myapp/LogInActivity.java
@@ -2,8 +2,10 @@ package com.MyGymRoutine.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,6 +19,8 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
 
         tvCreateAccount = findViewById(R.id.tvCreateAccount);
 
+        tvCreateAccount.setOnClickListener(this);
+
 
     }
 
@@ -24,7 +28,8 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.tvCreateAccount:
-
+                Intent i = new Intent(getApplicationContext(),Registro.class);
+                startActivity(i);
                 break;
         }
     }
