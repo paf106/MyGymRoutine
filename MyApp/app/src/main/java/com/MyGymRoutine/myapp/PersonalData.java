de6961a463afe36af9d52e365ce39715cf9efab2
paf106 <pabloavila106@gmail.com>
2021-04-20 15:07:08 +0200

    few methods added

--- MyApp/app/src/main/java/com/MyGymRoutine/myapp/PersonalData.java
+++ MyApp/app/src/main/java/com/MyGymRoutine/myapp/PersonalData.java
@@ -6,20 +6,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.MyGymRoutine.myapp.databinding.FragmentGymDataBinding;
+import com.MyGymRoutine.myapp.databinding.FragmentPersonalDataBinding;
 
 
 public class PersonalData extends Fragment {
 
-    Button btnSiguiente;
+    private FragmentPersonalDataBinding binding;
+
     public PersonalData() {
         // Required empty public constructor
     }
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,23 +36,51 @@ public class PersonalData extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_data, container, false);
+        binding = FragmentPersonalDataBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        btnSiguiente = view.findViewById(R.id.btnSiguiente);
+        // Saves the form data and forward to GymData
+        binding.btnSiguiente.setOnClickListener(v -> {
 
-        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            saveData();
+
+            GymData gymData = new GymData();
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.llFragments,gymData).commit();
+        });
+
+        binding.etPhone.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                GymData gymData = new GymData();
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .addToBackStack(null)
-                        .replace(R.id.llFragments,gymData).commit();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
+    }
+
+    private void saveData() {
+    String text = binding.etPhone.getText().toString();
+        Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+       // Toast.makeText(getActivity(), "He empezado", Toast.LENGTH_SHORT).show();
+
     }
 }
