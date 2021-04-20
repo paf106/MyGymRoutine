93a30f4143480ebafeae8e4972f1697cd337b08e
paf106 <pabloavila106@gmail.com>
2021-04-20 14:58:07 +0200

    Changed to data binding and Date Picker created

--- MyApp/app/src/main/java/com/MyGymRoutine/myapp/GymData.java
+++ MyApp/app/src/main/java/com/MyGymRoutine/myapp/GymData.java
@@ -1,5 +1,7 @@
 package com.MyGymRoutine.myapp;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,17 +12,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+
+import com.MyGymRoutine.myapp.databinding.FragmentGymDataBinding;
+import com.google.android.material.datepicker.MaterialDatePicker;
 
 
 public class GymData extends Fragment {
 
-    AutoCompleteTextView frecuenciaDeporte;
+    private FragmentGymDataBinding binding;
 
     public GymData() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,17 +33,42 @@ public class GymData extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gym_data, container, false);
+
+        binding = FragmentGymDataBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Build datepicker
+        MaterialDatePicker datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("Selecciona la fecha")
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .build();
+        
+        //When you click on the Birthday input
+        binding.etBirthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker.show(getFragmentManager(),"tag");
+            }
+        });
 
+        // When the user clicks OK in the dialog
+        datePicker.addOnPositiveButtonClickListener(selection -> {
+          //  String date = String.format(Locale.getDefault(), "%02d-%02d-%02d", day, month+1, year);
+            binding.etBirthday.setText(datePicker.getHeaderText());
+        });
 
-
+        binding.btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),MainScreen.class));
+            }
+        });
     }
 
     @Override
@@ -50,7 +78,13 @@ public class GymData extends Fragment {
         // Para el dropdown menu
         String[] options = getResources().getStringArray(R.array.frecuenciasDeportes);
         ArrayAdapter arrayAdapter = new ArrayAdapter(requireContext(),R.layout.dropdown_item,options);
-        frecuenciaDeporte = getView().findViewById(R.id.frecuenciaDeporte);
-        frecuenciaDeporte.setAdapter(arrayAdapter);
+        binding.frecuenciaDeporte.setAdapter(arrayAdapter);
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
