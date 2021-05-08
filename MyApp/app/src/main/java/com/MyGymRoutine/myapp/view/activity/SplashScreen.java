9cb00bd6efcfb0bcd92bf7e7a63a2eb72c276843
paf106 <pabloavila106@gmail.com>
2021-05-08 14:32:16 +0200

    line deleted

--- MyApp/app/src/main/java/com/MyGymRoutine/myapp/view/activity/SplashScreen.java
+++ MyApp/app/src/main/java/com/MyGymRoutine/myapp/view/activity/SplashScreen.java
@@ -16,7 +16,6 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        getSupportActionBar().hide();
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
