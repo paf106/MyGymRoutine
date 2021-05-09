d067142f4920fbb6e0a9ed3a1f840051a6dc38df
paf106 <pabloavila106@gmail.com>
2021-05-09 13:56:01 +0200

    Client class updated

--- MyApp/app/src/main/java/com/MyGymRoutine/myapp/data/model/Client.java
+++ MyApp/app/src/main/java/com/MyGymRoutine/myapp/data/model/Client.java
@@ -9,23 +9,18 @@ public class Client {
     private String username;
     private String password;
     private String email;
-    private Date birthday; // 00/00/0000
-    private int phoneNumber; // 000 000 000
+    private String birthday; // 00/00/0000
+    private int phoneNumber; // 000000000
     private int personalTrainerId;
-    private int height; // cm or m
-
-    public Client(String name, String surname, String username, String password, String email, Date birthday, int phoneNumber, int personalTrainerId) {
-        this.name = name;
-        this.surname = surname;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.birthday = birthday;
-        this.phoneNumber = phoneNumber;
-        this.personalTrainerId = personalTrainerId;
-    }
+    private double height; // cm or m
+    private double weight; // Kg
+    private String howOftenSport;
+    private String pathologies;
+    private String image; // Path of the image
 
     public Client(){ }
+
+
 
     public String getName() {
         return name;
@@ -45,10 +40,6 @@ public class Client {
 
     public String getEmail() {
         return email;
-    }
-
-    public Date getBirthday() {
-        return birthday;
     }
 
     public int getPhoneNumber() {
