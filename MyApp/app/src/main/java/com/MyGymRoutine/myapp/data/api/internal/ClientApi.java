e260d0b9d0ed8fabdae21d076e92c004df16ece9
paf106 <pabloavila106@gmail.com>
2021-05-09 13:58:03 +0200

    Related to api added

---
+++ MyApp/app/src/main/java/com/MyGymRoutine/myapp/data/api/internal/ClientApi.java
@@ -0,0 +1,29 @@
+package com.MyGymRoutine.myapp.data.api.internal;
+
+
+import com.MyGymRoutine.myapp.data.model.responses.ResponseRegister;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.POST;
+
+public interface ClientApi {
+    @POST("save.php")
+    Call<ResponseRegister> doRegister(
+            @Field("nombre") String nombre,
+            @Field("apellidos") String apellidos,
+            @Field("usuario") String usuario,
+            @Field("contrasena") String contrasena,
+            @Field("correoElectronico") String correoElectronico,
+            @Field("fechaNacimiento") String fechaNacimiento,
+            @Field("telefono") String telefono,
+            @Field("peso") String peso,
+            @Field("altura") String altura,
+            @Field("frecuencaDeporte") String frecuencaDeporte,
+            @Field("imagenRuta") String imagenRuta,
+            @Field("patologias") String patologias,
+            @Field("idPersonal") String idPersonal
+    );
+
+    @
+}
