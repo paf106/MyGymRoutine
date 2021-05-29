c29fe615f1ce65f61c6ff918b6f37adecf10397d
paf106 <pabloavila106@gmail.com>
2021-05-29 18:51:23 +0200

    Abrir galeria

--- MyApp/app/src/main/java/com/MyGymRoutine/myapp/view/activity/profile/ProfileFragment.java
+++ MyApp/app/src/main/java/com/MyGymRoutine/myapp/view/activity/profile/ProfileFragment.java
@@ -1,10 +1,16 @@
 package com.MyGymRoutine.myapp.view.activity.profile;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.provider.MediaStore;
@@ -16,6 +22,9 @@ import com.MyGymRoutine.myapp.data.model.Client;
 import com.MyGymRoutine.myapp.databinding.FragmentProfileBinding;
 import com.MyGymRoutine.myapp.view.activity.login.LogInActivity;
 import com.MyGymRoutine.myapp.view.components.utils.Preferences;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.jetbrains.annotations.NotNull;
 
 
 public class ProfileFragment extends Fragment {
@@ -60,10 +69,49 @@ public class ProfileFragment extends Fragment {
             startActivity(new Intent(getContext(), LogInActivity.class));
         });
         binding.ivPhotoProfile.setOnClickListener(v -> {
-            Intent i = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            i.setType("image/*");
-            startActivityForResult(i,101);
+            //Check permission
+            if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+
+                }else{
+                    ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},100);
+                }
+            }else{
+                openGallery();
+            }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+
+        if (requestCode == 100){
+            if (permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openGallery();
+            }else{
+                Snackbar.make(getView(), "Tienes que habilitar los permisos", Snackbar.LENGTH_LONG).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        if(requestCode == 100){
+            if (resultCode == Activity.RESULT_OK && data != null){
+                Uri photo = data.getData();
+                binding.ivPhotoProfile.setImageURI(photo);
+            }
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void openGallery(){
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        i.setType("image/*");
+        startActivityForResult(i,101);
     }
 
     @Override
